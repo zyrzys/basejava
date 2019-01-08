04a81e36d16c9a7cfb29a1576df48a639bdd6322
zyrzys <tonyckey1@gmail.com>
2019-01-08 02:08:16 +0200

    Home Work 01

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,60 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
 
     void clear() {
+        int storageSize = size();
+        for (int i = 0; i < storageSize; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(r.uuid)) {
+                System.out.println("Error! Resume with ID = " + r.uuid + " already exist!");
+                return;
+            }
+        }
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) return storage[i];
+        }
+        System.out.println("Warning! Resume with ID = " + uuid + " doest't exist.");
         return null;
     }
 
     void delete(String uuid) {
+        int storageSize = size();
+        for (int i = 0; i < storageSize; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[storageSize -1];
+                storage[storageSize-1] = null;
+                return;
+            }
+        }
+        System.out.println("Warning! Resume with ID = " + uuid + " doest't exist.");
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size());
     }
 
     int size() {
-        return 0;
+        int sizeCounter = 0;
+        for (Resume aStorage : storage) {
+            if (aStorage != null) sizeCounter++;
+        }
+        return sizeCounter;
     }
 }
